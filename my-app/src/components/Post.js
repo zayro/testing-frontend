298b49bce5678335f731ed5a2e13ca5e0de7d03a
marlon zayro <zayro8905@gmail.com>
2021-03-31 14:47:25 -0500

    Pruebas unitarias

---
+++ my-app/src/components/Post.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Form from "./Form";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  function createPost(data) {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((newPost) => setPosts([...posts, newPost]));
+  }
+
+  return (
+    <div>
+      <h1>Create new post</h1>
+      <Form submit={createPost} />
+
+      <section>
+        {posts.map((post) => (
+          <article key={post.id}>
+            <h2>Title - {post.title}</h2>
+            
+            <p> <span>{post.id}</span> content: {post.content}</p>
+          </article>
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default Posts;
